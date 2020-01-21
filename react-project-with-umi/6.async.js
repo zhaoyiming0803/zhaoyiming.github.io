2320da873a742345a49f75e1426e58f89fac0af7
zymfe <1047832475@qq.com>
2020-01-21 19:15:42 +0800

    update

--- react-project-with-umi/6.async.js
+++ react-project-with-umi/6.async.js
@@ -1,1 +1,1 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{RioY:function(e,n,s){"use strict";s.r(n);var t=s("cDcd"),a=s.n(t);class i extends a.a.Component{render(){return a.a.createElement("div",null,"this is index page")}}n["default"]=i}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{RioY:function(e,n,t){"use strict";t.r(n);var s=t("q1tI"),a=t.n(s);class i extends a.a.Component{render(){return a.a.createElement("div",null,"this is index page")}}n["default"]=i}}]);
